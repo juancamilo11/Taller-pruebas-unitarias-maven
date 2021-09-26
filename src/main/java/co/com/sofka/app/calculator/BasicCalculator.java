30ef5b0aa9d0054d56998ce6a911c5458a49b845
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-26 14:46:39 -0500

    Clase BasicCalculator terminada

---
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -0,0 +1,35 @@
+package co.com.sofka.app.calculator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class BasicCalculator {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    public Long sum(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 + number2;
+    }
+
+    public Long sub(Long number1, Long number2) {
+        logger.info( "Subtracting {} + {}", number1, number2 );
+        return number1 - number2;
+    }
+
+    public Long mult(Long number1, Long number2) {
+
+        logger.info( "Multiplicating {} + {}", number1, number2 );
+        return number1 * number2;
+    }
+
+    public Long div(Long number1, Long number2) {
+        if(number2 == 0) {
+            logger.error("number2 cannot be 0");
+        }
+        logger.info( "Dividing {} + {}", number1, number2 );
+        return number1 + number2;
+    }
+}
