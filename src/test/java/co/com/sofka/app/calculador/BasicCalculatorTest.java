500c81b7f6b1c7c2f2851f2fc0195339cded04bc
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-26 15:11:09 -0500

    Se completo exitosamente la prueba de la suma

--- src/test/java/co/com/sofka/app/calculador/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/calculador/BasicCalculatorTest.java
@@ -1,18 +1,26 @@
 package co.com.sofka.app.calculador;
 
 import co.com.sofka.app.calculator.BasicCalculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BasicCalculatorTest {
 
     private final BasicCalculator basicCalculator = new BasicCalculator();
 
+    @Test
     public void sum() {
         // Arrange
         Long number1 = 1L;
         Long number2 = 1L;
+        long expectedValue = 2L;
 
         // Act
-        basicCalculator.sum(number1, number2);
+        Long result = basicCalculator.sum(number1, number2);
+
+        assertEquals(expectedValue, result);
+
     }
 
 
